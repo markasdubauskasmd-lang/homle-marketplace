6bc13587ac0a0f117ffeb526d8efe885c6afde2b
gardirasan93-rgb <302596366+gardirasan93-rgb@users.noreply.github.com>
2026-07-29 12:32:24 +0100

    Add Cleaner Availability page

--- public/cleaner-registration.js
+++ public/cleaner-registration.js
@@ -1,4 +1,4 @@
-import { applicationStatusLabel, onboardingIcons, onboardingProgress } from "./cleaner-onboarding-steps.js?v=20260729-3";
+import { applicationStatusLabel, onboardingIcons, onboardingProgress } from "./cleaner-onboarding-steps.js?v=20260729-4";
 import { createCleanerPage, element, requestJson, setText } from "./cleaner-page.js?v=20260728-1";
 import { setupPersonalDetails } from "./cleaner-personal-details.js?v=20260728-1";
 import { setupBusinessDetails } from "./cleaner-business-details.js?v=20260728-1";
@@ -9,6 +9,7 @@ import { setupExperience } from "./cleaner-experience.js?v=20260728-1";
 import { setupReferences } from "./cleaner-references.js?v=20260729-1";
 import { setupInsurance } from "./cleaner-insurance.js?v=20260729-2";
 import { setupBanking } from "./cleaner-banking.js?v=20260729-1";
+import { setupAvailability } from "./cleaner-availability.js?v=20260729-1";
 
 function stepIcon(name) {
   const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
@@ -62,6 +63,10 @@ createCleanerPage("reg", async (context) => {
   }
   if (location.pathname === "/cleaner/banking") {
     await setupBanking(context);
+    return;
+  }
+  if (location.pathname === "/cleaner/availability") {
+    await setupAvailability(context);
     return;
   }
   const [profileResult, availabilityResult, payoutResult] = await Promise.allSettled([
